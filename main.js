8961197acf00396e2de811e6fb556c8a4b265fd0
Matt6oh9 <matheus.scaramal@escola.pr.gov.br>
2025-09-05 12:23:40 +0000

    onclick

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 const numeroSenha = document.querySelector('parametro-senha__texto');
+const botoes = document.querySelectorAll('parametro-senha__botao');
+
 let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho(){
+    if (tamanhoSenha > 1) {
+    tamanhoSenha --;
+    }
+}
+
+function diminuiTamanho(){
+    if (tamanhoSenha < 12) {
+    tamanhoSenha ++;
+    }
+}
+
 numeroSenha.textContent = tamanhoSenha;
