e9b5f1fe117a01ad5027e02c9b6242955bfd4026
Matt6oh9 <matheus.scaramal@escola.pr.gov.br>
2025-09-12 12:23:59 +0000

    checked

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const numeroSenha = document.querySelector('parametro-senha__texto');
 const botoes = document.querySelectorAll('parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos= '!@#$%¨&*()';
 
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
@@ -9,13 +14,40 @@ botoes[1].onclick = aumentaTamanho;
 function diminuiTamanho(){
     if (tamanhoSenha > 1) {
     tamanhoSenha --;
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
     }
 }
 
 function diminuiTamanho(){
     if (tamanhoSenha < 12) {
     tamanhoSenha ++;
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
     }
 }
 
 numeroSenha.textContent = tamanhoSenha;
+
+function geraSenha(){
+    let alfabeto = '';
+    let senha = '';
+    if (checkbox[0].checked){
+        alfabeto = alfabeto + letrasMaiusculas
+    }
+    if (checkbox[1].checked){
+        alfabeto = alfabeto + letrasMinusculas
+    }
+    if (checkbox[2].checked){
+        alfabeto = alfabeto + numeros
+    }
+    if (checkbox[3].checked){
+        alfabeto = alfabeto + simbolos
+    }
+    for (let i = 0; 1 < tamanhoSenha; i++){
+        let numeroAleatório = Math.random() * letrasMaiusculas.length;
+        numeroAleatório = Math.floor(numeroAleatório);
+        senha = senha + alfabeto[numeroAleatório];
+
+    }
+}
