a1788245f608c9934c726c2c8d74976c36be97c4
Matt6oh9 <matheus.scaramal@escola.pr.gov.br>
2025-09-16 10:48:17 +0000

    16/09/2025

--- main.js
+++ main.js
@@ -1,29 +1,34 @@
-const numeroSenha = document.querySelector('parametro-senha__texto');
-const botoes = document.querySelectorAll('parametro-senha__botao');
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+const botoes = document.querySelectorAll('.parametro-senha__botao');
 const campoSenha = document.querySelector('#campo-senha');
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
 const numeros = '0123456789';
 const simbolos= '!@#$%¨&*()';
-
+const checkbox = document.querySelectorAll('.checkbox');
 let tamanhoSenha = 12;
+
+for (i = 0; i < checkbox.length; i++) {
+        checkbox[i].onclick = geraSenha;
+    }
+
 numeroSenha.textContent = tamanhoSenha;
-botoes[0].onclick = diminuiTamanho;
-botoes[1].onclick = aumentaTamanho;
+botoes[0].onclick = aumentaTamanho();
+botoes[1].onclick = diminuiTamanho();
 
 function diminuiTamanho(){
     if (tamanhoSenha > 1) {
-    tamanhoSenha --;
-    numeroSenha.textContent = tamanhoSenha;
-    geraSenha();
+        tamanhoSenha --;
+        numeroSenha.textContent = tamanhoSenha;
+        geraSenha();
     }
 }
 
-function diminuiTamanho(){
-    if (tamanhoSenha < 12) {
-    tamanhoSenha ++;
-    numeroSenha.textContent = tamanhoSenha;
-    geraSenha();
+function aumentaTamanho(){
+    if (tamanhoSenha < 20) {
+        tamanhoSenha ++;
+        numeroSenha.textContent = tamanhoSenha;
+        geraSenha();
     }
 }
 
